9ade1cd305c3e13c63c246cb2e3341c7553fd8cf
MajedAlqudah <majedjameel063@gmail.com>
2024-01-09 12:28:19 +0300

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,14 +1,14 @@
 <?php
 
-$sname = "sql211.infinityfree.com";
-$uname = "if0_35750089";
-$passord = "2ujfGULeYOmmrqx";
+$sname = "localhost";
+$uname = "root";
+$passord = "";
 
-$db_name = "if0_35750089_conference_information_system";
+$db_name = "conference_information_system";
 
-$conn = mysqli_connect($sname ,$uname ,$passord ,$db_name);
+$connection = mysqli_connect($sname ,$uname ,$passord ,$db_name);
 
-if(!$conn)
+if(!$connection)
 {
     echo "connection failed";
 }
